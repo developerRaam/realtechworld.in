32181381ccf7812dc24bc6f1a942b2a407074cd3
Ramnarayan <rnm312000@gmail.com>
2023-09-13 00:09:56 +0530

    update dsa

--- dsa_questions/admin.py
+++ dsa_questions/admin.py
@@ -6,12 +6,17 @@ from dsa_questions.models import *
 admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Tags)
+admin.site.register(Platform)
 
 class CompanyTags(admin.StackedInline):
     model = SelectTags
     extra = 0
 
+class PlatformSelection(admin.StackedInline):
+    model = SelectPlatform
+    extra = 0
+
 class DsaQuestionAdmin(admin.ModelAdmin):
-    inlines = [CompanyTags] # multiple images reference
+    inlines = [CompanyTags,PlatformSelection] # multiple images reference
     list_display = ('question_name','cat_id','sub_cat_id')
 admin.site.register(DsaQuestion,DsaQuestionAdmin)
--- dsa_questions/models.py
+++ dsa_questions/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     class Meta:
-        verbose_name_plural = "Category"
+        verbose_name_plural = "difficulty"
     
 class SubCategory(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
@@ -19,13 +19,15 @@ class SubCategory(models.Model):
     def __str__(self):
         return self.name
     class Meta:
-        verbose_name_plural = "Sub Category"
+        verbose_name_plural = "Topics"
+
 
 class DsaQuestion(models.Model):
     question_name = models.CharField(max_length=300, blank=True, null=True)
     slug = AutoSlugField(populate_from="question_name",unique=True,null=True, always_update=True)
     cat_id = models.ForeignKey(Category, verbose_name="Category",null=True,blank=True, on_delete=models.SET_NULL)
     sub_cat_id = models.ForeignKey(SubCategory, verbose_name="SubCategory",null=True,blank=True, on_delete=models.SET_NULL)
+    url = models.URLField(max_length=200,null=True,blank=True)
     description = HTMLField(null=True, blank=True)
     c_language = HTMLField(null=True, blank=True)
     cpp_language = HTMLField(null=True, blank=True)
@@ -37,6 +39,19 @@ class DsaQuestion(models.Model):
         return self.question_name
     class Meta:
         verbose_name_plural = "DSA Question"
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=50)
+    slug = AutoSlugField(populate_from="name",unique=True,null=True, always_update=True)
+    def __str__(self):
+        return self.name
+
+class SelectPlatform(models.Model):
+    question_id = models.ForeignKey(DsaQuestion, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Platform, verbose_name="Platform", on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = 'Choose Platform' 
 
 
 class Tags(models.Model):
